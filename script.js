ebe49779727c739766cab01ab2385bb4e35cdf09
skibidibidop <beltran.mapb@gmail.com>
2023-02-19 20:22:05 +0800

    Add function for comparing choices of computer and player

--- script.js
+++ script.js
@@ -23,16 +23,73 @@ function assignComputerSelection(computerNumber) // computerNumber will come fro
 
 function playRound(compChoice, playerChoice)
 {
+    /*
+    Return 7 for tie. Arbitrary starting number, I just wanted to stay away from 1 and 0.
+    Return 8 for player score
+    Return 9 for computer score
+    */
+    if (compChoice === playerChoice)
+    {
+        console.log('Tie!');
+        return 7; 
+    }
 
+    if (compChoice === 'rock')
+    {
+        if (playerChoice === 'paper')
+        {
+            console.log('You win! Paper beats Rock.');
+            return 8;
+        }
+        else if (playerChoice === 'scissors')
+        {
+            console.log('You lose! Rock beats Scissors');
+            return 9;
+        }
+    }
+    else if (compChoice=== 'paper')
+    {
+        if (playerChoice === 'rock')
+        {
+            console.log('You lose! Paper beats Rock.');
+            return 9;
+        }
+        else if (playerChoice === 'scissors')
+        {
+            console.log('You win! Scissors beats Paper.');
+            return 8;
+        }
+    }
+    else
+    {
+        if (playerChoice === 'rock')
+        {
+            console.log('You win! Rock beats Scissors.');
+            return 8;
+        }
+        else if (playerChoice === 'paper')
+        {
+            console.log('You lose! Rock beats Scissors.');
+            return 9;
+        }
+    }
 }
 
-// Generate random number between 1 to 3, then assign rock, paper, or scissors to computerSelection depending
-// on the result
+// Main program flow below
+
+    /*  
+    Generate random number between 1 to 3, then assign rock, paper, or scissors to computerSelection depending
+    on the result
+    */
 
 let randomNumber = getComputerChoice(1, 4);
 let computerSelection = assignComputerSelection(randomNumber);
 
-// Convert all the characters of player input to lower case 
+    // Convert all the characters of player input to lower case 
 
 let playerInput = prompt('Rock, paper, or scissors?', '');
 let playerSelection = playerInput.toLowerCase();
+
+    // Compare computerSelection and playerSelection
+
+let roundResult = playRound(computerSelection, playerSelection);

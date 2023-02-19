5c063079bfdd9efa44e58ce970fd3d616bd12de0
skibidibidop <beltran.mapb@gmail.com>
2023-02-19 19:58:42 +0800

    Put computerSelection value assignment inside function

--- script.js
+++ script.js
@@ -5,27 +5,34 @@ function getComputerChoice(min, max)
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+function assignComputerSelection(computerNumber) // computerNumber will come from the random number generator
+{
+    if (computerNumber === 1)
+    {
+        return 'rock';
+    }
+    else if (computerNumber === 2)
+    {
+        return 'paper';
+    }
+    else if (computerNumber === 3)
+    {
+        return 'scissors';
+    }
+}
+
+function playRound(compChoice, playerChoice)
+{
+
+}
+
 // Generate random number between 1 to 3, then assign rock, paper, or scissors to computerSelection depending
 // on the result
 
 let randomNumber = getComputerChoice(1, 4);
-let computerSelection = 0;
+let computerSelection = assignComputerSelection(randomNumber);
 
-if (randomNumber === 1)
-{
-    computerSelection = 'rock';
-}
-else if (randomNumber === 2)
-{
-    computerSelection = 'paper';
-}
-else if (randomNumber === 3)
-{
-    computerSelection = 'scissors';
-}
-
-
+// Convert all the characters of player input to lower case 
 
 let playerInput = prompt('Rock, paper, or scissors?', '');
 let playerSelection = playerInput.toLowerCase();
-

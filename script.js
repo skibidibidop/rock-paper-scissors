baf94fce3f1d0f20aacbcd3d936db8fc2df3d153
skibidibidop <beltran.mapb@gmail.com>
2023-02-19 18:55:19 +0800

    Add value assignment to computerSelection by random no. gen

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 'use strict';
 
+function getComputerChoice(min, max)
+{
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+// Generate random number between 1 to 3, then assign rock, paper, or scissors to computerSelection depending
+// on the result
+
+let randomNumber = getComputerChoice(1, 4);
+let computerSelection = 0;
+
+if (randomNumber === 1)
+{
+    computerSelection = 'rock';
+}
+else if (randomNumber === 2)
+{
+    computerSelection = 'paper';
+}
+else if (randomNumber === 3)
+{
+    computerSelection = 'scissors';
+}
+
+
+
 let playerInput = prompt('Rock, paper, or scissors?', '');
 let playerSelection = playerInput.toLowerCase();
 
